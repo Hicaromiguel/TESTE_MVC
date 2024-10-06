1f501fe84653fdc35b96743a82cfd2704bdbb8ec
HI <153924670+Hicaromiguel@users.noreply.github.com>
2024-10-06 15:17:42 -0300

    Add files via upload

---
+++ MVC_teste/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.text.View;
+import java.awt.*;
+import Control.Controle;
+
+public class Main {
+    static Controle mycontrole = new Controle();
+    public static void main(String[] args) {
+        mycontrole.Entrada();
+    }
+}
